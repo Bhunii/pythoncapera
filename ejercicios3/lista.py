2aa9bf4905ee3cc02e22dc50184c535f0a49138a
stiben <bscapera9@soy.sena.edu.co>
2023-05-05 00:11:53 -0500

    se termina lista de ejercicios3 pero falta comentar codigos del profe

--- ejercicios3/lista.py
+++ ejercicios3/lista.py
@@ -1,4 +1,5 @@
 import random
+
 lista=[]
 sum, total, prod=0, 0, 0
 tam=random.randint(10,20)
@@ -8,6 +9,7 @@ for i in range(tam):
     lista.append(num)
 print(lista)
 
+#suma total y promedio
 for i in range(len(lista)):
     sum+=lista[i]
     total=sum
@@ -15,6 +17,7 @@ for i in range(len(lista)):
 print(f'la suma total es de: {total}')
 print(f'la promedio es de: {prod}')
 
+#mayor y menor
 mayor = 0
 menor = 1000000
 for i in lista:
@@ -25,13 +28,58 @@ for i in lista:
 print(f'el mayor es: {mayor}')
 print(f'el menor es: {menor}')
 
+#moda
+k=len(lista)
+cant=0
+for i in range(k):
+    cont=0
+    for j in range(k):
+        if lista[i] == lista [j]:
+            cont= cont+1
+    if cont>cant:
+        cant = cont 
+        moda = lista[i]
+if cant == 1:
+    print('no hay moda')
+else:
+    print(f'la moda es {moda} se repite {cant}')
 
-'''
-moda = 0
+#mediana
+m=lista
+m.sort()
+#for i in range(m):
+if tam%2 == 0:
+    y = (tam // 2) 
+    j = y-1
+    med = (m[y] + m[j]) / 2
+    #print(m)
+    #print(m[y])
+    #print(m[j])
+    print(f'la mediana es {med}')
+if tam%2 != 0:
+    div=tam//2
+    #v=div-1
+    #print(m)
+    #print (div)
+    print(f'la mediana es {m[div]}')
+
+#desviacion estandar
+resta = []
+cont2=0
+sum2, total2 = 0, 0
 for i in lista:
-    cont=
-    for j in lista+1:
-        if i==j:
-            moda=j
-print(f'la moda es: {moda}')
-'''
+    r=i - prod
+    resta.append(r)
+for i in range(len(resta)):
+    resta[i]=resta[i]**2
+for i in range(len(resta)):
+    sum2+=resta[i]
+    cont2=cont2+1
+    total2=sum2
+total3=sum2//tam
+#print(total3)
+r=total3
+x=0
+while x * x <= total3:
+    x+=0.0001
+print(f'la desviacion estandar es {x:.3f}')
