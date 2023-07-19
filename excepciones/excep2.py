3a02d526489ad5d87f9d410267cc8160f0116ec7
stiben <bscapera9@soy.sena.edu.co>
2023-07-19 10:43:36 -0500

    excepciones 1.5)

--- excepciones/excep2.py
+++ excepciones/excep2.py
@@ -16,17 +16,14 @@ def cuadraticaTryExcept(a, b, c, signo):
             temp2=(-(b)+temp)/(2*(a))
         return temp2        
     except ValueError:
-        print('Valor no acepatable')
-        return None
+        return 'Valor no acepatable'
     except ZeroDivisionError:
-        print('No se puede dividir por cero')
-        return None
+        return 'No se puede dividir por cero'
     except:
-        print('Error inesperado')
-        return None
+        return 'Error inesperado'
 
 aux=cuadratica(5,6,1,'-')
 print(aux)
-aux2=cuadraticaTryExcept(1,2,3,'a')
+aux2=cuadraticaTryExcept(5,6,1,'-')
 print(aux2)
 
