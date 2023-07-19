c04ea98a7d68316d2eb7394cb18005e58d1c67d6
stiben <bscapera9@soy.sena.edu.co>
2023-07-19 10:37:20 -0500

    excepciones 1.2

--- excepciones/excep2.py
+++ excepciones/excep2.py
@@ -1,8 +1,32 @@
 import math
-def cuadratica(a, b, c):
-    
-    temp=math.sqrt(b** - 4 * a * c)
-    return temp
+def cuadratica(a, b, c, signo):    
+    temp=math.sqrt((b**2) - 4 * a * c)
+    if signo == '-':
+        temp2=(-(b)-temp)/(2*(a))
+    if signo == '+':
+        temp2=(-(b)+temp)/(2*(a))
+    return temp2
 
-c=cuadratica(2,3,4)
-print(c)
+def cuadraticaTryExcept(a, b, c, signo):    
+    try:
+        temp=math.sqrt((b**2) - 4 * a * c)
+        if signo == '-':
+            temp2=(-(b)-temp)/(2*(a))
+        if signo == '+':
+            temp2=(-(b)+temp)/(2*(a))
+        return temp2        
+    except ValueError:
+        print('Valor no acepatable')
+        return None
+    except ZeroDivisionError:
+        print('No se puede dividir por cero')
+        return None
+    except:
+        print('Error inesperado')
+        return None
+
+aux=cuadratica(5,6,1,'-')
+print(aux)
+aux2=cuadraticaTryExcept(1,2,3,'a')
+print(aux2)
+
